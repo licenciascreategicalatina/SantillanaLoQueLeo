e94916c1414b102e5444c4e1f65ba65559c2861c
rodinson17 <rstombe@unimayor.edu.co>
2021-05-03 14:12:59 -0500

    cambios en estilos

--- resources/js/views/HomeDayOne.vue
+++ resources/js/views/HomeDayOne.vue
@@ -204,59 +204,65 @@
                             </div>
 
                             <vs-dialog width="550px" not-center v-model="agendaBook">
-                                <a id="pdf-download" href="/book/Lo que leo.pdf" download="programa">
-                                    <div id="content-dialog-book" class="con-content" >
-                                        <h3 class="text-center color-white font-weight-bold">
-                                            Programa
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
-                                                <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/>
-                                            </svg>
-                                        </h3>
-                                        <p class="text-center">
-                                            <img src="/image/img-dialog.png" alt="img dialog">
-                                        </p>
-                                        <div class="row">
-                                            <div class="col-4 text-center color-white">
-                                                <h4>3:30</h4>
-                                                <span>Bienvenida</span>
+                                <vs-tooltip color="#b1935e" border-thick>
+                                    <a id="pdf-download" href="/book/Lo que leo.pdf" download="programa">
+                                        <div id="content-dialog-book" class="con-content" >
+                                            <h3 class="text-center color-white font-weight-bold">
+                                                Programa
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
+                                                    <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+                                                </svg>
+                                            </h3>
+                                            <p class="text-center">
+                                                <img src="/image/img-dialog.png" alt="img dialog">
+                                            </p>
+                                            <div class="row">
+                                                <div class="col-4 text-center color-white">
+                                                    <h4>3:30</h4>
+                                                    <span>Bienvenida</span>
+                                                </div>
+                                                <div class="col-4 text-center">
+                                                    <h4>4:30</h4>
+                                                    <span>Conferencia magistral <br>Julieta Pinasco</span>
+                                                </div>
+                                                <div class="col-4 text-center color-white">
+                                                    <h4>6:45</h4>
+                                                    <span>Taller <br>Raquel Cuperman</span>
+                                                </div>
                                             </div>
-                                            <div class="col-4 text-center">
-                                                <h4>4:30</h4>
-                                                <span>Conferencia magistral <br>Julieta Pinasco</span>
+                                            <div class="row mt-2">
+                                                <div class="col-4 text-center">
+                                                    <h4>3:35</h4>
+                                                    <span>Cata de libros</span>
+                                                </div>
+                                                <div class="col-4 text-center color-white">
+                                                    <h4>5:15</h4>
+                                                    <span>Taller <br>Alejandra Jaramillo</span>
+                                                </div>
+                                                <div class="col-4 text-center">
+                                                    <h4>7:30</h4>
+                                                    <span>Cierre</span>
+                                                </div>
                                             </div>
-                                            <div class="col-4 text-center color-white">
-                                                <h4>6:45</h4>
-                                                <span>Taller <br>Raquel Cuperman</span>
+                                            <div class="row mt-2">
+                                                <div class="col-4 text-center color-white">
+                                                    <h4>4:15</h4>
+                                                    <span>¿Qué pasó en la cata? <br>Los profesores lo cuentan</span>
+                                                </div>
+                                                <div class="col-4 text-center">
+                                                    <h4>6:00</h4>
+                                                    <span>Conferencia magistral <br>Eduardo Escallón</span>
+                                                </div>
+                                                <div class="col-4 text-center">
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="row mt-2">
-                                            <div class="col-4 text-center">
-                                                <h4>3:35</h4>
-                                                <span>Cata de libros</span>
-                                            </div>
-                                            <div class="col-4 text-center color-white">
-                                                <h4>5:15</h4>
-                                                <span>Taller <br>Alejandra Jaramillo</span>
-                                            </div>
-                                            <div class="col-4 text-center">
-                                                <h4>7:30</h4>
-                                                <span>Cierre</span>
-                                            </div>
-                                        </div>
-                                        <div class="row mt-2">
-                                            <div class="col-4 text-center color-white">
-                                                <h4>4:15</h4>
-                                                <span>¿Qué pasó en la cata? <br>Los profesores lo cuentan</span>
-                                            </div>
-                                            <div class="col-4 text-center">
-                                                <h4>6:00</h4>
-                                                <span>Conferencia magistral <br>Eduardo Escallón</span>
-                                            </div>
-                                            <div class="col-4 text-center">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
+                                    </a>
+
+                                    <template #tooltip>
+                                        descargar programa
+                                    </template>
+                                </vs-tooltip>
                             </vs-dialog>
 
                             <div class="card animate__animated animate__slideInRight animate__slower">
@@ -333,8 +339,10 @@
                 activeModal: false,
                 heightIframeBook: false,
                 agendaBook: false,
-                changeBackground: true,
-                viewHoverBooks: false,
+                //changeBackground: true,
+                changeBackground: false,
+                //viewHoverBooks: false,
+                viewHoverBooks: true,
                 openChatIcon: false,
                 listBooks: [],
                 bookSelect: null,
@@ -848,6 +856,9 @@
     }
     div#content-agenda-book button {
         background: #9f793d;
+    }
+    div#content-agenda-book button:hover {
+        box-shadow: 0px 10px 20px -10px rgb(193 59 43);
     }
     #content-dialog-book {
         background: #b1935e;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,15 +47,21 @@
                         <li class="nav-item ml-3 {{ request()->is('preescolar') ? 'active' : '' }}">
                             <a class="nav-link nav-link-book" href="{{ route('day-one') }}">Preescolar</a>
                         </li>
-                        <li class="nav-item ml-3 disabled {{ request()->is('primaria') ? 'active' : '' }}">
+                        <li class="nav-item ml-3 {{ request()->is('primaria') ? 'active' : '' }}">
+                            <a class="nav-link nav-link-book" href="{{ route('day-two') }}">Primaria</a>
+                        </li>
+                        <li class="nav-item ml-3 {{ request()->is('secundaria') ? 'active' : '' }}">
+                            <a class="nav-link nav-link-book" href="{{ route('day-three') }}">Secundaria</a>
+                        </li>
+                        {{-- <li class="nav-item ml-3 disabled {{ request()->is('primaria') ? 'active' : '' }}">
                             <a class="nav-link nav-link-book disabled" href="{{ route('day-two') }}">Primaria</a>
                         </li>
                         <li class="nav-item ml-3 disabled {{ request()->is('secundaria') ? 'active' : '' }}">
                             <a class="nav-link nav-link-book disabled" href="{{ route('day-three') }}">Secundaria</a>
-                        </li>
-                        <li class="nav-item ml-3" style="opacity: 0;">
+                        </li> --}}
+                        {{-- <li class="nav-item ml-3" style="opacity: 0;">
                             <a class="nav-link nav-link-book" href="{{ route('import-view') }}">Import</a>
-                        </li>
+                        </li> --}}
                     </ul>
 
                     <!-- Right Side Of Navbar -->
