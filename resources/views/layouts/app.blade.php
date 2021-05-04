32c8a599a5aca3346959aff99a7ad0e6e99cde82
rodinson17 <rstombe@unimayor.edu.co>
2021-05-03 20:51:57 -0500

    quitar la opcion de subir datos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,9 +68,9 @@
                         <li class="nav-item ml-3" data-toggle="tooltip" data-placement="bottom" title="Disponible 6 de mayo">
                             <span class="nav-link-book" style="display: block;padding: 0.5rem 1rem;">Secundaria</span>
                         </li>
-                        <li class="nav-item ml-3" style="opacity: 0;">
+                       {{--  <li class="nav-item ml-3" style="opacity: 0;">
                             <a class="nav-link nav-link-book" href="{{ route('import-view') }}">Import</a>
-                        </li>
+                        </li> --}}
                     </ul>
 
                     <!-- Right Side Of Navbar -->
